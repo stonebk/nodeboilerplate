bea455ce8bfb188617275a793735068d7821e843
Brian Stone <briankstone@gmail.com>
2012-02-12 22:43:11 -0500

    Adding optimist argument parsing.

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var express = require('express'),
-    app = module.exports = express.createServer();
+    app = module.exports = express.createServer(),
+    opts = require(__dirname + '/config/opts.js');
 
 require(__dirname + '/config/env.js')(express, app);
 require(__dirname + '/routes')(app);
 
-app.listen(3000);
+app.listen(opts.port);
 console.log("Express server listening on port %d in %s mode",
             app.address().port, app.settings.env);
---
+++ config/opts.js
@@ -0,0 +1,6 @@
+module.exports = require('optimist')
+        .usage('Usage: $0 --port [port]')
+        .alias('port', 'p')
+        .describe('port', 'Port number for the Express application.')
+        .default('port', 3000)
+        .argv;
