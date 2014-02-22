07f6cf5e694be9bdb85b64950a937e9af0cad75d
Brian Stone <briankstone@gmail.com>
2014-02-22 11:35:18 -0500

    Render using mustache templates.

--- boilerplate/config/env.js
+++ boilerplate/config/env.js
@@ -1,16 +1,30 @@
-var util = require(__dirname + '/../libs/util.js');
+var util = require(__dirname + '/../libs/util.js'),
+    mustache = require('mu2');
 
 module.exports = function (express, app) {
 
     // Common configuration
     app.configure(function () {
 
-        // Configure jQuery template engine
-        express.version = require('express/package.json').version;
+        // Configure mustache template engine
+        mustache.root = __dirname + '/../views';
         app.set('views', __dirname + '/../views');
-        app.set('view engine', 'jqtpl');
-        app.set('layout', true);
-        app.engine('jqtpl', require('jqtpl').__express);
+        app.set('view engine', 'mustache');
+        app.engine('mustache', function (path, options, fn) {
+            var buffer = [];
+
+            // Always recompile in development
+            if (app.settings.env === 'development') {
+                mustache.clearCache();
+            }
+            mustache.compileAndRender(path, options).on('data', function (data) {
+                buffer.push(data);
+            }).on('end', function () {
+                fn(null, buffer.join(''));
+            }).on('error', function (e) {
+                fn(e);
+            });
+        });
 
         app.use(app.router);
 
